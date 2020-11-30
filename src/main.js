15035969a368c6eb750f99e1dbbd3bb8bdd242e5
Seadog <baosj@createw.com>
2020-11-30 16:15:17 +0800

    [ID1000331]开发者-开发者申请入驻事项提交上线申请上传用户手册文件名称较长时名称超出了弹窗（附件）

--- src/main.js
+++ src/main.js
@@ -2,10 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import ContentMenu from "v-contextmenu";
+import "v-contextmenu/dist/index.css";
+import ElementUI from "element-ui";
+import "@/style/init.styl";
 
 Vue.config.productionTip = false;
+Vue.use(ElementUI, { size: "small" });
+Vue.use(ContentMenu);
 
-new Vue({
+export default new Vue({
     router,
     store,
     render: h => h(App)
